afebb020fd272e1ed9250cb9d580014ab30ee55e
George Egorian <gosha.egorian@gmail.com>
2020-12-13 00:47:54 +0300

    Add name options. Improve composition API.

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,87 @@
-import type { Matcher, DuoRefinementTypeCompositionOptions, RefinementTypeOptions, SingleRefinementTypeCompositionOptions, Name } from './types';
+import {
+  Matcher,
+  PairRefinementTypeCompositionOptions,
+  RefinementTypeOptions,
+  SingleRefinementTypeCompositionOptions,
+  Name,
+  Data,
+  isRefinementTypeOperatorOptions,
+  isPairRefinementTypeCompositionExtended,
+  isName,
+  isMatcher,
+} from './types';
 
 class RefinementType {
   private matcher: Matcher;
-  name: Name | undefined;
+  name: Name;
+  left?: RefinementType;
+  right?: RefinementType;
 
-  constructor({ matcher, name }: RefinementTypeOptions) {
-    this.matcher = matcher;
-    this.name = name;
+  constructor(props: RefinementTypeOptions) {
+    this.name = props.name;
+
+    if (isMatcher(props)) {
+      this.matcher = props;
+    } else {
+      this.matcher = props.matcher;
+    }
+
+    if (isRefinementTypeOperatorOptions(props)) {
+      const { left, right } = props;
+      this.left = left;
+      this.right = right;
+    }
   }
 
-  test(data: any): boolean {
+  test(data: Data): boolean {
     return this.matcher(data);
   }
-
-  match(data: any) {
+  match(data: Data): boolean {
     return this.test(data);
   }
 
-  and({ type, name }: DuoRefinementTypeCompositionOptions): RefinementType {
-    return new RefinementType({ name, matcher: (data: any) => this.matcher(data) && type.matcher(data) });
+  and(props: PairRefinementTypeCompositionOptions): RefinementType {
+    let type: RefinementType, name: Name;
+    if (isPairRefinementTypeCompositionExtended(props)) {
+      type = props.type;
+      name = props.name;
+    } else {
+      type = props;
+    }
+    return new RefinementType({ name, matcher: RefinementType.andOperator, left: this, right: type });
   }
-  or({ type, name }: DuoRefinementTypeCompositionOptions): RefinementType {
-    return new RefinementType({ name, matcher: (data: any) => this.matcher(data) || type.matcher(data) });
+  or(props: PairRefinementTypeCompositionOptions): RefinementType {
+    let type: RefinementType, name: Name;
+    if (isPairRefinementTypeCompositionExtended(props)) {
+      type = props.type;
+      name = props.name;
+    } else {
+      type = props;
+    }
+    return new RefinementType({ name, matcher: RefinementType.orOperator, left: this, right: type });
   }
-  not({ name }: SingleRefinementTypeCompositionOptions): RefinementType {
-    return new RefinementType({ name, matcher: (data: any) => !this.matcher(data) });
+  not(props?: SingleRefinementTypeCompositionOptions): RefinementType {
+    const name: Name = isName(props) ? props : props.name;
+    return new RefinementType({ name, matcher: RefinementType.notOperator, left: this });
+  }
+
+  static andOperator(this: RefinementType, data: Data): boolean {
+    if (this.left && this.right) {
+      return this.left.test(data) && this.right.test(data);
+    }
+    return false;
+  }
+  static orOperator(this: RefinementType, data: Data): boolean {
+    if (this.left && this.right) {
+      return this.left.test(data) || this.right.test(data);
+    }
+    return false;
+  }
+  static notOperator(this: RefinementType, data: Data): boolean {
+    if (this.left) {
+      return !this.left.test(data);
+    }
+    return false;
   }
 };
 
